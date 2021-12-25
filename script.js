79aea83dac0c1b702b59a177c480c7c74c19afed
y.b.l** <69856522+yblui@users.noreply.github.com>
2021-12-25 20:18:37 +0800

    3m

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-function click(element){
-    
+var playerOnePlaying = true, xCoords=0;
+
+function clk(element) {
+    if(!xCoords) xCoords=Number(element.dataset.xcoordinate);
+    if(Number(element.dataset.xcoordinate)!=xCoords) return;
+    element.style.backgroundColor = "gray";
+}
+
+function togglePlayer(){
+    playerOnePlaying = !playerOnePlaying;
+    xCoords = 0;
 }
