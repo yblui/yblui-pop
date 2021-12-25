b77c455c7cb30662914756aad82952cbb016ec46
y.b.l** <69856522+yblui@users.noreply.github.com>
2021-12-25 21:05:01 +0800

    3m

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-var playerOnePlaying = true, xCoords=0;
+var playerOnePlaying = true, xCoords = 0, dotsRemaining = 30, playerOneName = "Player 1", playerTwoName = "Player 2";
 
 function clk(element) {
-    if(!xCoords) xCoords=Number(element.dataset.xcoordinate);
-    if(Number(element.dataset.xcoordinate)!=xCoords) return;
+    if (!xCoords) xCoords = Number(element.dataset.xcoordinate);
+    if (Number(element.dataset.xcoordinate) != xCoords) return;
+    if (element.style.backgroundColor == "gray") return;
     element.style.backgroundColor = "gray";
+    dotsRemaining--;
+    if (!dotsRemaining) document.getElementById("player").innerText = "Game Over."
 }
 
-function togglePlayer(){
+function togglePlayer() {
+    if (!xCoords) return;
     playerOnePlaying = !playerOnePlaying;
     xCoords = 0;
+    if (playerOnePlaying) document.getElementById("player").innerText = playerOneName + " Playing";
+    else document.getElementById("player").innerText = playerTwoName + " Playing";
+}
+
+function play(p1name, p2name) {
+    playerOneName = p1name;
+    playerTwoName = p2name;
+    document.getElementById("settings").style.top = "-110%";
 }
