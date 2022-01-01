971bf2c882c4a2f8b65971ca039facc0830e7b16
y.b.l** <69856522+yblui@users.noreply.github.com>
2022-01-01 22:46:23 +0800

    1u3m

--- script.js
+++ script.js
@@ -7,37 +7,36 @@ function clk(element) {
     if (element.style.backgroundColor == "gray") return;
     element.style.backgroundColor = "gray";
     dotsRemaining--;
-    if (!dotsRemaining) {
-        if (playerOnePlaying) {
-            document.getElementById("player").innerText = playerTwoName + " Won";
-            playerTwoScore++;
-        } else {
-            document.getElementById("player").innerText = playerOneName + " Won";
-            playerOneScore++;
-        }
-        document.getElementById("playerOnePoint").innerText=playerOneScore;
-        document.getElementById("playerTwoPoint").innerText=playerTwoScore;
-        document.getElementById("points").style.left="0";
-        setTimeout(function () {
-            document.getElementById("points").style.left = "100%";
-        }, 3000)
-        if (playerOneScore >= firstTo && playerOneScore >= playerTwoScore + winBy) {
-            document.getElementById("winnerName").innerText = playerOneName
-            document.getElementById("winner").style.left = "0";
-            document.getElementById("settings").style.top = "0";
-        } else if (playerTwoScore >= firstTo && playerTwoScore >= playerOneScore + winBy) {
-            document.getElementById("winnerName").innerText = playerTwoName;
-            document.getElementById("winner").style.left = "0";
-            document.getElementById("settings").style.top = "0";
-        }
-        setTimeout(function () {
-            document.getElementById("winner").style.left = "100%";
-        }, 3000)
-        xCoords = 0;
-        dotsRemaining = 30;
-        for (var x of document.getElementsByTagName("div")) {
-            if (x.style.backgroundColor == "gray") x.style.backgroundColor = "";
-        }
+    if (dotsRemaining) return;
+    if (playerOnePlaying) {
+        document.getElementById("player").innerText = playerTwoName + " Won";
+        playerTwoScore++;
+    } else {
+        document.getElementById("player").innerText = playerOneName + " Won";
+        playerOneScore++;
+    }
+    document.getElementById("playerOnePoint").innerText = playerOneScore;
+    document.getElementById("playerTwoPoint").innerText = playerTwoScore;
+    document.getElementById("points").style.left = "0";
+    setTimeout(function () {
+        document.getElementById("points").style.left = "100%";
+    }, 3000)
+    if (playerOneScore >= firstTo && playerOneScore >= playerTwoScore + winBy) {
+        document.getElementById("winnerName").innerText = playerOneName
+        document.getElementById("winner").style.left = "0";
+        document.getElementById("settings").style.top = "0";
+    } else if (playerTwoScore >= firstTo && playerTwoScore >= playerOneScore + winBy) {
+        document.getElementById("winnerName").innerText = playerTwoName;
+        document.getElementById("winner").style.left = "0";
+        document.getElementById("settings").style.top = "0";
+    }
+    setTimeout(function () {
+        document.getElementById("winner").style.left = "100%";
+    }, 3000)
+    xCoords = 0;
+    dotsRemaining = 30;
+    for (var x of document.getElementsByTagName("div")) {
+        if (x.style.backgroundColor == "gray") x.style.backgroundColor = "";
     }
 }
 
@@ -56,6 +55,8 @@ function togglePlayer() {
 function play(p1name, p2name, ft, wb) {
     playerOneName = p1name;
     playerTwoName = p2name;
+    document.getElementById("player1").innerText = p1name;
+    document.getElementById("player2").innerText = p2name;
     firstTo = ft;
     winBy = wb;
     document.getElementById("settings").style.top = "-110%";
